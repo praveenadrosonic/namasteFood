14418d2272214b0b14034ae79e150b56c2e382da
Praveen Kumar <praveen.adrosonic@gmail.com>
2023-12-31 11:13:17 +0530

    added shimmer ui

--- src/components/Body.js
+++ src/components/Body.js
@@ -8,7 +8,7 @@ const Body = () => {
 
     const topFilterToggle = () => {
         if(topRatedText === 'Top Rated Restaurants :') {
-            setTopRestaurantList(restaurantList.filter( (restaurant) => { return (restaurant.info.avgRating >= 4.0) }).sort((a, b) => { return (a.info.avgRating - b.info.avgRating) *-1}));
+            setTopRestaurantList(restaurantList.filter( (restaurant) => { return (restaurant?.info?.avgRating >= 4.0) }).sort((a, b) => { return (a?.info?.avgRating - b?.info?.avgRating) *-1}));
             setTopRatedText('Hide Top Rated Restaurants :');
         } else {
             setTopRestaurantList([]);
@@ -24,17 +24,21 @@ const Body = () => {
         const data = await fetch(RESTAURANT_API_URL);
         const jsonData = await data.json();
         console.log(jsonData);
-        setRestaurantList(jsonData.data.cards[1].card.card.gridElements.infoWithStyle.restaurants);
+        setRestaurantList(jsonData?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
+    }
+
+    if(restaurantList.length === 0) {
+        return <span className="loader"></span>;
     }
 
     return (
         <>
             <div className="search-container">Search</div>
             <button className="top-rated" onClick={topFilterToggle}><h2>{topRatedText}</h2></button>
-            <div className="restaurant-container top-rated-restaurants-container">{ restaurantTopList.map( (restaurant) => <RestaurantCard key={restaurant.info.id} restaurantDetails={restaurant}/> ) }</div>
+            <div className="restaurant-container top-rated-restaurants-container">{ restaurantTopList.map( (restaurant) => <RestaurantCard key={restaurant?.info?.id} restaurantDetails={restaurant}/> ) }</div>
             <h2>All Restaurants:</h2>
             <div className="restaurant-container">
-                { restaurantList.map( (restaurant) => <RestaurantCard key={restaurant.info.id} restaurantDetails={restaurant}/> ) }
+                { restaurantList.map( (restaurant) => <RestaurantCard key={restaurant?.info?.id} restaurantDetails={restaurant}/> ) }
             </div>
         </>
     )
